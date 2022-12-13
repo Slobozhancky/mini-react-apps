fcd02f65436d9db455f9acbb124d7b30778eebc3
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-12-13 17:01:57 +0200

    init: init app quiz

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../scss/app.scss";
+
+function App() {
+    let [open, setOpen] = React.useState(false);
+
+    function toggleModal() {
+        setOpen(!open);
+    }
+
+    function Modal({ open, setOpen }) {
+        return (
+            <div className={`modal-wrapper hidden ${open ? "show" : ""}`}>
+                <h3>Hello world</h3>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quam illo esse odit natus sunt odio incidunt quo placeat
+                    officia cumque!
+                </p>
+                <div className="cross" onClick={() => setOpen(false)}>
+                    X
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="app-wrapper">
+            <button className="btn" onClick={() => toggleModal()}>
+                Show modal
+            </button>
+
+            {/* Приклад рендерингу за допомогою компонента */}
+            <Modal open={open} setOpen={setOpen} />
+        </div>
+    );
+}
+
+export default App;
